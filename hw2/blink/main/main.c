3ef38814a5a0aa1be0b9839f8df8145800da37ed
Vova Babushkin <babushkinvova05@gmail.com>
2026-02-01 00:28:58 +0300

    init commit

---
+++ hw2/blink/main/main.c
@@ -0,0 +1,15 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+
+#define LED GPIO_NUM_2
+void app_main(void) {
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+  uint32_t led_on = 0;
+  while (true) {
+    led_on = !led_on;
+    gpio_set_level(LED, led_on);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
