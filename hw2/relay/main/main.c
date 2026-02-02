8ed02f6f3f29b859460fbd056744bf84dcfd3869
Vova Babushkin <babushkinvova05@gmail.com>
2026-02-03 02:31:33 +0300

    relay

---
+++ hw2/relay/main/main.c
@@ -0,0 +1,18 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED GPIO_NUM_18
+#define RELAY GPIO_NUM_21
+void app_main(void) {
+  gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+  gpio_set_direction(RELAY, GPIO_MODE_INPUT_OUTPUT);
+  uint32_t ticks = 0;
+  while (true) {
+    gpio_set_level(LED, ticks % 2);
+    if (ticks % 20 == 0)
+      gpio_set_level(RELAY, !gpio_get_level(RELAY));
+    ticks++;
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
